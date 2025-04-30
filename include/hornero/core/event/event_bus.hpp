68d2f98aca5e5ca45b42d5d3b3a09dfaeb758cdc
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-04-30 17:11:39 -0300

    Add event_bus.hpp

---
+++ include/hornero/core/event/event_bus.hpp
@@ -0,0 +1,65 @@
+
+#ifndef HORNERO_EVENT_BUS_HPP
+#define HORNERO_EVENT_BUS_HPP
+
+#include <functional>
+#include <typeindex>
+#include <memory>
+#include <unordered_map>
+#include <queue>
+
+#include <hornero/core/event/event.hpp>
+#include <hornero/core/event/generic_event.hpp>
+
+namespace hornero::core::event
+{
+    template <EventType Tp>
+    using EventCallback = std::function<void(const Tp &)>;
+
+    using BaseEventCallback = EventCallback<BaseEvent>;
+    using GenericEventCallback = EventCallback<GenericEvent>;
+
+    class EventBus
+    {
+    public:
+        using subscribers_map_type =
+            std::unordered_map<std::type_index, std::vector<BaseEventCallback>>;
+
+        using generic_subscribers_map_type =
+            std::unordered_map<GenericEvent::hash_type, std::vector<GenericEventCallback>>;
+
+        using event_queue_type =
+            std::queue<std::unique_ptr<BaseEvent>>;
+
+        template <GenericEventType Tp>
+        static void Subscribe(const std::string &type, const GenericEventCallback &callback);
+
+        template <NonGenericEventType Tp>
+        static void Subscribe(const BaseEventCallback &callback);
+
+        static void SubscribeGeneric(const std::string &type, const GenericEventCallback &callback);
+
+        template <EventType Tp, typename... Args>
+        static void Post(Args... args);
+
+        static void DispatchQueued();
+
+        template <GenericEventType Tp>
+        static void InmediateDispatch(const Tp &event);
+
+        template <NonGenericEventType Tp>
+        static void InmediateDispatch(const Tp &event);
+
+    private:
+        EventBus() = default;
+        ~EventBus() = default;
+
+        static subscribers_map_type subscribers;
+        static generic_subscribers_map_type generic_subscribers;
+        static event_queue_type events;
+    };
+} // namespace hornero::core::event
+
+#include "event_bus.ipp"
+
+#endif // HORNERO_EVENT_BUS_HPP
