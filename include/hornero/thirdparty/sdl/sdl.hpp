cebf87da211a1e20235a5e1ab4b20f099c75007e
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-04-30 17:18:06 -0300

    Add sdl.hpp

---
+++ include/hornero/thirdparty/sdl/sdl.hpp
@@ -0,0 +1,14 @@
+
+#ifndef HORNERO_THIRDPARTY_SDL_SDL_HPP
+#define HORNERO_THIRDPARTY_SDL_SDL_HPP
+
+#include <SDL3/SDL.h>
+
+namespace hornero::thirdparty::sdl
+{
+    void Init();
+
+    void Quit();
+} // namespace hornero::thirdparty::sdl
+
+#endif // HORNERO_THIRDPARTY_SDL_SDL_HPP
