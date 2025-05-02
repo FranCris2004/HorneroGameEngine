6f054267f660a3594dcf260c412091c56421169e
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-05-02 02:45:01 -0300

    implemented using types

--- include/hornero/utils/hash/hasher.hpp
+++ include/hornero/utils/hash/hasher.hpp
@@ -6,10 +6,9 @@
 
 namespace hornero::utils::hash
 {
-    template <template <typename HasherValueTp> class HasherTp>
+    template <typename HasherValueTp>
     struct HasherTraits
     {
-        using type = HasherTp;
         using hash_value_type = HasherValueTp;
     };
 
@@ -17,32 +16,33 @@ namespace hornero::utils::hash
     class Hasher
     {
     public:
-        using typename HasherTraits<HasherValueTp>::type;
-        using typename HasherTraits<HasherValueTp>::hash_value_type;
+        using type = Hasher<HasherValueTp>;
+        using traits = HasherTraits<HasherValueTp>;
+        using hash_value_type = traits::hash_value_type;
 
         virtual ~Hasher() = default;
 
-        virtual hash_value_type hash(const ByteView &bytes) const = 0;
+        virtual hash_value_type hash(const memory::ByteView &bytes) const = 0;
 
-        inline hash_value_type operator()(const ByteView &bytes) const
+        inline hash_value_type operator()(const memory::ByteView &bytes) const
         {
             return hash(bytes);
         }
 
         inline hash_value_type operator()(const std::string &str) const
         {
-            return hash(ByteView(str));
+            return hash(memory::ByteView(str));
         }
 
         inline hash_value_type operator()(const char *str) const
         {
-            return hash(ByteView(str));
+            return hash(memory::ByteView(str));
         }
 
         template <typename U>
         inline hash_value_type operator()(const U &value) const
         {
-            return hash(ByteView(value));
+            return hash(memory::ByteView(value));
         }
     };
 } // namespace hornero::utils::hash
